22bc70a126145422c522dbb4403332b997f6a5df
Petrik Diak <diak@petrik.hu>
2025-05-09 12:02:06 +0200

    Kerdes class

---
+++ BraunRoland_Milliomos/Kerdesek.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BraunRoland_Milliomos
+{
+	internal class Kerdesek
+	{
+		string kerdes;
+		private List<string> valaszok;
+		char helyes;
+		string kategoria;
+
+		public Kerdesek(string kerdes, List<string> valaszok, char helyes, string kategoria)
+		{
+			this.kerdes = kerdes;
+			this.valaszok = valaszok;
+			this.helyes = helyes;
+			this.kategoria = kategoria;
+		}
+
+		public string Kerdes { get => kerdes; set => kerdes = value; }
+		public List<string> Valaszok { get => valaszok; set => valaszok = value; }
+		public char Helyes { get => helyes; set => helyes = value; }
+		public string Kategoria { get => kategoria; set => kategoria = value; }
+	}
+}
--- BraunRoland_Milliomos/Program.cs
+++ BraunRoland_Milliomos/Program.cs
@@ -4,7 +4,18 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			StreamReader sr = new("kerdes.txt");
+			while (!sr.EndOfStream)
+			{
+				string sor = sr.ReadLine();
+				string[] adatok = sor.Split(';');
+				Console.WriteLine($"Kérdés: {adatok[0]}");
+				Console.WriteLine($"A: {adatok[1]}");
+				Console.WriteLine($"B: {adatok[2]}");
+				Console.WriteLine($"C: {adatok[3]}");
+				Console.WriteLine($"D: {adatok[4]}");
+				Console.WriteLine($"Helyes válasz: {adatok[5]}");
+				Console.WriteLine("------------------------------");
 		}
 	}
 }
